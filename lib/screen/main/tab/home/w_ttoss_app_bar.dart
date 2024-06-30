59531222ce078d9a287ed76f820ed6fd2bc8d95d
sssun <98_0509@naver.com>
2024-06-30 22:57:01 +0900

    화면 루팅

--- lib/screen/main/tab/home/w_ttoss_app_bar.dart
+++ lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:flutter/material.dart';
 
 class TtossAppBar extends StatefulWidget {
@@ -27,9 +28,8 @@ class _TtossAppBarState extends State<TtossAppBar> {
           width10,
           Tap(
             onTap: () {
-              setState(() {
-                _showRedDot = !_showRedDot;
-              });
+              //알림 화면으로 리다이렉트
+              Nav.push(NotificationScreen());
             },
             child: Stack(
               children: [
