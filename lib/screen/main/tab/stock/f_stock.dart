7763069fdfbc0bdfc5eeaf91e545255f24011d6f
sssun <98_0509@naver.com>
2024-07-04 22:09:52 +0900

    tabBar

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -2,6 +2,8 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../common/widget/w_image_button.dart';
+import 'tab/f_my_stock.dart';
+import 'tab/f_todays_discovery.dart';
 
 class StockFragment extends StatefulWidget {
   const StockFragment({super.key});
@@ -10,7 +12,19 @@ class StockFragment extends StatefulWidget {
   State<StockFragment> createState() => _StockFragmentState();
 }
 
-class _StockFragmentState extends State<StockFragment> {
+class _StockFragmentState extends State<StockFragment> with SingleTickerProviderStateMixin {
+
+  int currentIndex = 0;
+  late final tabController = TabController(length: 2, vsync: this);
+  // 변수의 초기화 과정을 생성자가 만들어지는 순간이 아닌, late 키워드가 붙은 변수가 필요해지는 순간에 초기화가 이루어짐.
+
+  // TabController? tabController;
+  //
+  // @override
+  // void initState() {
+  //   tabController = TabController(length: 2, vsync: this);
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +58,10 @@ class _StockFragmentState extends State<StockFragment> {
             children: [
               title,
               tabBar,
+              if (currentIndex == 0)
+                const MyStockFragment()
+              else
+                const TodaysDiscoveryFragment(),
               myAccount,
               height20,
               myStocks,
@@ -65,7 +83,28 @@ class _StockFragmentState extends State<StockFragment> {
       3919.29.toComma().text.size(13).bold.color(context.appColors.plus).make(),
     ],
   ).pOnly(left: 20);
-  Widget get tabBar => Placeholder();
+
+
+  Widget get tabBar => Column(
+    children: [
+      TabBar(
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        labelStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        labelPadding: const EdgeInsets.symmetric(vertical: 20),
+        indicatorPadding: const EdgeInsets.symmetric(horizontal: 20),
+        indicatorColor: Colors.white,
+        controller: tabController,
+        tabs:[
+        '내 주식'.text.make(),
+        '오늘의 발견'.text.make(),
+      ],),
+      const Line(),
+    ],
+  );
   Widget get myAccount => Placeholder();
   Widget get myStocks => Placeholder();
 
